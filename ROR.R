7ddb0a222021a3dc1ded8591b2542a9332ea67e1
canhgiacduoc <mrcouragem1dkh@gmail.com>
2017-01-03 11:06:39 +0100

    Update ROR.R

--- ROR.R
+++ ROR.R
@@ -7,10 +7,12 @@ OR95=function(x){
   }
 
 ### Plot ROR with CI 95%
-PlotROR=function(x,y,ymin,ymax){
-  ggplot(aes(x,y)) + 
-  geom_errorbar(aes(ymin, ymax), colour="black", width=.2) +
-  geom_point( size=3, shape=21, fill="white") + # 21 is filled circle
+plotROR=function(df){
+  localenv = environment()#If fun is a function or a formula then environment(fun) returns the environment associated with that function or formula.
+  # If fun is NULL then the current evaluation environment is returned.
+  ggplot(df,aes(x=Cepha), environment = localenv ) + 
+  geom_errorbar(aes(ymin=lower, ymax=upper), colour="black", width=.2) +
+  geom_point( aes(y=OR),size=3, shape=21, fill="white") + # 21 is filled circle
   xlab(NULL) +
   ylab("Odd Ratios Adjusted") +
   geom_hline(yintercept=1, color="red", alpha=0.4,lwd=1.5) +
