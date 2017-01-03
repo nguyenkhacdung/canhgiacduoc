2407e6ff5ab342445434e2cd10072418aab61586
canhgiacduoc <mrcouragem1dkh@gmail.com>
2017-01-03 10:28:00 +0100

    Update ROR.R

--- ROR.R
+++ ROR.R
@@ -1,4 +1,18 @@
+#FUNCTION#
+
+### OR calculation with CI 95%
 OR95=function(x){
   return(exp(cbind(OR=coef(x), confint(x))))
   }
- 
+
+### Plot ROR with CI 95%
+PlotROR=function(x,y,ymin,ymax){
+  ggplot(aes(x,y)) + 
+  geom_errorbar(aes(ymin, ymax), colour="black", width=.2) +
+  geom_point( size=3, shape=21, fill="white") + # 21 is filled circle
+  xlab(NULL) +
+  ylab("Odd Ratios Adjusted") +
+  geom_hline(yintercept=1, color="red", alpha=0.4,lwd=1.5) +
+  coord_flip()+
+  theme_bw() + ylim(0,8) 
+}
